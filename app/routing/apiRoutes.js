be342bc667cf77d4de892004f0517a848b73797a
Chiara DeLucia <chiaramdelucia@gmail.com>
2017-03-05 14:21:58 -0500

    Adding route logic and page rendering (all in server, routes need to be separated in to their own files). Also, setting up basic html structure for testing.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,9 @@
+//Friends (DATA)
+// =============================================================
+// var friends = require('../data/friends.js');
+// // var server = require('../../server.js')(app);
+// const express = require('express');
+// var app = express();
+
+
+
--- server.js
+++ server.js
@@ -3,11 +3,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require ('path');
+// var api = require('./app/routing/apiRoutes.js');
 
 
 //Initialize Express and local host
 // =============================================================
-const app = express();
+var app = express();
 const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
@@ -17,6 +18,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static(__dirname));
+
+//Friends (DATA)
+// =============================================================
+var friends = require('./app/data/friends.js');
+
+
+//API Routes
+// =============================================================
+app.get('/api/friends', function (req, res){
+	// json.parse(friends)
+	res.json(friends);
+	// res.json(true);
+	console.log(friends);
+	// console.log(req.body);
+	
+});
+
+app.post('/api/friends', function (req, res){
+	friends.push(req.body);
+	
+});
+
+//HTML Routes
+//=============================================================
+app.get('/survey', function (req, res){
+
+	res.sendFile(path.join(__dirname, '/app/public/survey.html'));
+
+});
+
+app.get('/', function (req, res){
+
+	res.sendFile(path.join(__dirname, '/app/public/home.html'));
+
+});
 
 
 //Listen on port 3000
